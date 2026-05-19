a48166a121bee70cb3be1cf9dc543276b815f2be
Harshit <honeyff463@gmail.com>
2026-05-19 20:55:50 +0530

    Cleared Dependencies

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,18 +13,23 @@ origins = [
 
     "http://localhost:5173",
 
-    "https://knowledge-system-km63nxw28-harshit-goyal-s-projects1.vercel.app/dashboard",
+    "https://knowledge-system-9b7ji2c-harshit-goyal-s-projects1.vercel.app",
 
 ]
-# ✅ CORS Middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=origins,
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+
+)
 # ✅ Include Routes
 app.include_router(router)
 
